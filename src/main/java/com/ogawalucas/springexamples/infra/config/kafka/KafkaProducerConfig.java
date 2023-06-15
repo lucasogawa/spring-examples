fbf9fcc06b0174b78abc300d2f87554fb59021fa
Lucas Akio Ogawa <ogawalucas@gmail.com>
2023-06-15 06:51:22 -0300

    Configuring Kafka Producer and Consumer

---
+++ src/main/java/com/ogawalucas/springexamples/infra/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,37 @@
+package com.ogawalucas.springexamples.infra.config.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value = "${kafka.producer.bootstrap-server}")
+    private String bootstrapServer;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        var props = new HashMap<String, Object>();
+
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
